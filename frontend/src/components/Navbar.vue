10ec6bb6293f56b73f5655349ab02ae1cb0d4c4a
Mo'Ventor <galbeedmok@gmail.com>
2020-04-20 23:37:37 +0800

    style(navbar): Orders navbar and alters comments.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -9,10 +9,9 @@
       <v-toolbar-title>SomBudget</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
-     <v-btn text color='primary' small>Signup</v-btn>
-    <v-btn text @click="routePush('/')" color='primary' small >General</v-btn>
-    <v-btn text color='primary' small> Tasks</v-btn>
+    <v-btn text @click="routePush('/')" color='primary' small >General analysis</v-btn>
+    <v-btn text color='primary' small>Sectional Analysis</v-btn>
+    <v-btn text color='primary' small>Commentary</v-btn>
     </v-toolbar>
   </v-card>
 </template>
